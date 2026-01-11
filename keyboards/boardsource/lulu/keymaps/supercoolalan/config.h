23f9d20847b8d449b1457ade819d88ba7c53959a
Alan Sandoval <alan@etherflow.io>
2026-01-10 21:52:57 -0600

    Strip to vanilla - test build

--- keyboards/boardsource/lulu/keymaps/supercoolalan/config.h
+++ keyboards/boardsource/lulu/keymaps/supercoolalan/config.h
@@ -2,9 +2,3 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
-
-// Enable reactive/splash RGB effects
-#define RGB_MATRIX_KEYPRESSES
-
-// Set default effect to multisplash (rainbow ripples)
-#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_MULTISPLASH
